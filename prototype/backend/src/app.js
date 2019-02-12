5ba6d626e5d11af568e2e09301aeef48bd445c3e
Michael Gabler <mich.gabler@gmx.de>
2019-02-12 13:30:30 +0100

    Add initial projects for prototype

---
+++ prototype/backend/src/app.js
@@ -0,0 +1,32 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var routes = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  res.status(err.status || 500);
+});
+
+module.exports = app;
