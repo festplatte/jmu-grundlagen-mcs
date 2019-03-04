667f65e00fe94a89f3fd88bd78b3474cb9653d1a
Bastian Hedenkamp <bastian.hedenkamp@stud-mail.uni-wuerzburg.de>
2019-03-04 07:43:01 +0100

    product information is now stored in produkte.json add to cart button added showing price and other info

--- prototype/webapp/src/components/ProductPreview/ProductPreview.js
+++ prototype/webapp/src/components/ProductPreview/ProductPreview.js
@@ -1,18 +1,48 @@
 import React from "react";
 import "./ProductPreview.scss";
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
+import ArrowRight from "@material-ui/icons/ArrowRight";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
 
 class ProductPreview extends React.Component {
+  static propTypes = {
+    product: PropTypes.object.isRequired,
+    onAddToCart: PropTypes.func
+  };
   render() {
+    const { product } = this.props;
     return (
       <div className="product-preview">
-        <h3>{this.props.title}</h3>
+        <h3>{product.title}</h3>
         <Grid container spacing={16}>
           <Grid item xs={4}>
-            <img src={process.env.PUBLIC_URL + "/produktbilder/" + this.props.image} alt="Produktbild" width="100%"></img>
+            <img
+              src={process.env.PUBLIC_URL + "/produktbilder/" + product.image}
+              alt="Produktbild"
+              width="100%"
+            />
           </Grid>
           <Grid item xs={8}>
-            <p>{this.props.description}</p>
+            <p>{product.description}</p>
+            <h2 className="pricetag">
+              {product.price
+                .toFixed(2)
+                .toString()
+                .replace(".", ",") + " €"}
+            </h2>
+            <p>zzgl. Versand ab 4,95 €</p>
+            <h3 class="availability">Auf Lager</h3>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={this.props.onAddToCart}
+            >
+              In den Einkaufswagen
+              <ArrowRight />
+              <ShoppingCart />
+            </Button>
           </Grid>
         </Grid>
       </div>
--- prototype/webapp/src/views/Product.js
+++ prototype/webapp/src/views/Product.js
@@ -3,35 +3,21 @@ import { connect } from "react-redux";
 import ProductPreview from "../components/ProductPreview/ProductPreview";
 import { addToCart } from "../redux/actions/cartActions";
 import { Button } from "@material-ui/core";
-
-const products = [{ title: "P1", description: "Ein Smartphone", tags: "", price: "", image: "phone-a.svg" },
-{ title: "P2", description: "Ein Smartphone", tags: "", price: "", image: "phone-b.svg" },
-{ title: "P3", description: "Eine Grafikkarte", tags: "", price: "", image: "gpu-a.svg" },
-{ title: "P4", description: "Eine Grafikkarte", tags: "", price: "", image: "gpu-b.svg" },
-{ title: "P5", description: "Arbeitsspeicher 8 GB DDR4", tags: "", price: "", image: "ram-a.svg" },
-{ title: "P6", description: "Arbeitsspeicher 8 GB DDR4", tags: "", price: "", image: "ram-b.svg" }];
+import products from "../produkte";
 
 class Product extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <div>
         <h1>Produktseite</h1>
         {products.map((product, i) => (
-          <ProductPreview key={i} title={product.title} description={product.description} image={product.image} />
+          <ProductPreview
+            key={i}
+            product={product}
+            onAddToCart={() => this.props.addToCart(product)}
+          />
         ))}
-        <Button
-          onClick={() => {
-            this.props.addToCart({
-              id: 1,
-              title: "my product",
-              price: 105.99,
-              amount: 3
-            });
-          }}
-        >
-          in den Einkaufswagen
-        </Button>
-      </React.Fragment>
+      </div>
     );
   }
 }
