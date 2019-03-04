929d03cd28a8105d918ce572882e5cbbc81ce32b
mg <mich.gabler@gmx.de>
2019-03-04 22:35:47 +0100

    Fix date format

--- prototype/webapp/src/utils/moment.js
+++ prototype/webapp/src/utils/moment.js
@@ -2,6 +2,6 @@ import moment from "moment";
 import "moment/locale/de";
 
 moment.locale("de");
-moment.defaultFormat = "dddd, MMMM Do YYYY, hh:mm:ss";
+moment.defaultFormat = "dddd, MMMM Do YYYY, HH:mm:ss";
 
 export default moment;
