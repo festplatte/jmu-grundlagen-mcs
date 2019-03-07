0cbca4ed064927ab0a535044d777c9446c35e7bd
Bastian Hedenkamp <bastian.hedenkamp@stud-mail.uni-wuerzburg.de>
2019-03-07 14:02:01 +0100

    kategorien mit bug

--- prototype/webapp/src/views/Home.js
+++ prototype/webapp/src/views/Home.js
@@ -9,6 +9,11 @@ import Grid from "@material-ui/core/Grid";
 import StarIcon from "@material-ui/icons/StarBorder";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
+import categories from "../kategorien";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 const styles = theme => ({
   heroContent: {
@@ -32,127 +37,78 @@ const styles = theme => ({
   }
 });
 
-const tiers = [
-  {
-    title: "Free",
-    price: "0",
-    description: [
-      "10 users included",
-      "2 GB of storage",
-      "Help center access",
-      "Email support"
-    ],
-    buttonText: "Sign up for free",
-    buttonVariant: "outlined"
-  },
-  {
-    title: "Pro",
-    subheader: "Most popular",
-    price: "15",
-    description: [
-      "20 users included",
-      "10 GB of storage",
-      "Help center access",
-      "Priority email support"
-    ],
-    buttonText: "Get started",
-    buttonVariant: "contained"
-  },
-  {
-    title: "Enterprise",
-    price: "30",
-    description: [
-      "50 users included",
-      "30 GB of storage",
-      "Help center access",
-      "Phone & email support"
-    ],
-    buttonText: "Contact us",
-    buttonVariant: "outlined"
-  }
-];
-
 class Home extends React.Component {
   static propTypes = {
     classes: PropTypes.object.isRequired
   };
 
+  state = {
+    anchorEl: null
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  getMainCategories(categories, anchorEl) {
+    var ret;
+    categories.forEach(element => {
+      ret = (
+        <>
+          {ret}
+          <Grid item xs={6} sm={3} md={1}>
+            <Button
+              aria-owns={anchorEl ? "simple-menu" : undefined}
+              aria-haspopup="true"
+              onClick={this.handleClick}
+            >
+              {Object.keys(element)[0]}
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={this.handleClose}
+            >
+              {this.getSubCategories(Object.values(element)[0])}
+            </Menu>
+          </Grid>
+        </>
+      );
+    });
+    return ret;
+  }
+
+  getSubCategories(subcategories) {
+    var ret;
+    subcategories.forEach(element => {
+      //console.log(element);
+      ret = (
+        <>
+          {ret}
+          <MenuItem onClick={this.handleClose}>{element}</MenuItem>
+        </>
+      );
+    });
+    return ret;
+  }
+
   render() {
     const { classes } = this.props;
+    const { anchorEl } = this.state;
 
     return (
-      <React.Fragment>
-        {/* Hero unit */}
-        <div className={classes.heroContent}>
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="textPrimary"
-            gutterBottom
-          >
-            Pricing
-          </Typography>
-          <Typography
-            variant="h6"
-            align="center"
-            color="textSecondary"
-            component="p"
-          >
-            Quickly build an effective pricing table for your potential
-            customers with this layout. It&apos;s built with default Material-UI
-            components with little customization.
-          </Typography>
-        </div>
-        {/* End hero unit */}
-        <Grid container spacing={40} alignItems="flex-end">
-          {tiers.map(tier => (
-            // Enterprise card is full width at sm breakpoint
-            <Grid
-              item
-              key={tier.title}
-              xs={12}
-              sm={tier.title === "Enterprise" ? 12 : 6}
-              md={4}
-            >
-              <Card>
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: "center" }}
-                  subheaderTypographyProps={{ align: "center" }}
-                  action={tier.title === "Pro" ? <StarIcon /> : null}
-                  className={classes.cardHeader}
-                />
-                <CardContent>
-                  <div className={classes.cardPricing}>
-                    <Typography component="h2" variant="h3" color="textPrimary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="textSecondary">
-                      /mo
-                    </Typography>
-                  </div>
-                  {tier.description.map(line => (
-                    <Typography variant="subtitle1" align="center" key={line}>
-                      {line}
-                    </Typography>
-                  ))}
-                </CardContent>
-                <CardActions className={classes.cardActions}>
-                  <Button
-                    fullWidth
-                    variant={tier.buttonVariant}
-                    color="primary"
-                  >
-                    {tier.buttonText}
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </React.Fragment>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Grid container spacing={24} alignItems="flex-end">
+            {this.getMainCategories(categories, anchorEl)}
+          </Grid>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
